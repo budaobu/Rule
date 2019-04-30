f7acc1f521ccd915e21462824333b2c485f21387
Choler <43330069+Choler@users.noreply.github.com>
2019-04-30 14:46:08 +0800

    Add files via upload

---
+++ KWPlayer.js
@@ -0,0 +1,9 @@
+var obj = JSON.parse(body);
+obj.data.isNewUser = 2;
+obj.data.vipLuxuryExpire = 1738717749000;
+obj.data.isYearUser = 2;
+obj.data.vipmExpire = 1738717749000;
+obj.data.vipOverSeasExpire = 1738717749000;
+obj.data.vipExpire = 1738717749000;
+obj.data.vip3Expire = 1738717749000;
+JSON.stringify(obj);
--- WeChat.js
+++ WeChat.js
@@ -1,3 +1,3 @@
-var obj = JSON.parse(body); // 对 response body 进行 JSON 解析得到 Object
-delete obj['advertisement_info']; // 删除 'advertisement_info' 字段
-JSON.stringify(obj); // 进行 JSON 编码并作为结果返回
+var obj = JSON.parse(body);
+delete obj['advertisement_info'];
+JSON.stringify(obj);
