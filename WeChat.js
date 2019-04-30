ef5e907cbe83e0002ff5f3958fc68ad20027c271
Choler <43330069+Choler@users.noreply.github.com>
2019-04-30 14:14:05 +0800

    Add files via upload

--- WeChat.js
+++ WeChat.js
@@ -1,3 +1,3 @@
-var obj = JSON.parse(body);
-obj.advertisement_info = [];
-result = JSON.stringify(obj);
+var obj = JSON.parse(body); // 对 response body 进行 JSON 解析得到 Object
+delete obj['advertisement_info']; // 删除 'advertisement_info' 字段
+JSON.stringify(obj); // 进行 JSON 编码并作为结果返回
