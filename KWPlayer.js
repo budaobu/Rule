d5355c30ab0d52e436fd61db9ce7cea9db2e88cb
Choler <43330069+Choler@users.noreply.github.com>
2019-05-09 23:01:26 +0800

    Add files via upload

--- KWPlayer.js
+++ KWPlayer.js
@@ -3,13 +3,7 @@ let path2 = '/music.pay';
 
 if (url.indexOf(path1) != -1) {
     var obj = JSON.parse(body);
-    obj.data.isNewUser = 2;
-    obj.data.vipLuxuryExpire = 1738717749000;
-    obj.data.isYearUser = 2;
-    obj.data.vipmExpire = 1738717749000;
-    obj.data.vipOverSeasExpire = 1738717749000;
-    obj.data.vipExpire = 1738717749000;
-    obj.data.vip3Expire = 1738717749000;
+    obj.data = JSON.parse('{"time":"1556095726751","vipExpire":1738717749000,"biedSong":"0","isYearUser":2,"vipmExpire":1738717749000,"biedAlbum":"0","vipOverSeasExpire":1738717749000,"vip3Expire":1738717749000,"vipLuxuryExpire":1738717749000,"isNewUser":2}');
     result = JSON.stringify(obj);
 }
 
