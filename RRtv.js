bd5f198403959e05e2ba72e065dd5318366de207
Choler <43330069+Choler@users.noreply.github.com>
2019-05-09 08:57:17 +0800

    Add files via upload

--- RRtv.js
+++ RRtv.js
@@ -1,16 +1,4 @@
-let path1 = '/ad/getAll';
-let path2 = '/user/profile';
-
-if (url.indexOf(path1) != -1) {
-    var obj = JSON.parse(body);
-    delete obj['data'];
-    result = JSON.stringify(obj);
-}
-
-if (url.indexOf(path2) != -1) {
-    var obj = JSON.parse(body);
-    obj.data.user.medalList = JSON.parse('[{"name":"原画","endTime":"2033-12-31 23:59:59","imgUrl":"http://img.rr.tv/screenshot/20171108/o_1510128764030.png","id":1}]');
-    obj.data.user.privilegeList = JSON.parse('[{"effectObject":"video","action":"play","func":"originalPainting","description":"解锁原画","icon":"jiesuoyuanhua","endTime":1671718942000},{"effectObject":"coin","action":"sign","func":"5","description":"签到额外银币+5","icon":"qiandaoyinbi","endTime":1671718942000},{"effectObject":"growth","action":"play","func":"0.4","description":"经验值加成+40%","icon":"jingyanzhijiacheng","endTime":1671718942000},{"effectObject":"video","action":"play","func":"noLimit","description":"看剧无限制","icon":"kanjuwuxianzhi","endTime":1671718942000},{"effectObject":"video","action":"play","func":"noAd","description":"看剧无告","icon":"kanjuwuguanggao","endTime":1671718942000}]');
-    result = JSON.stringify(obj);
-}
-result;
+var obj = JSON.parse(body);
+obj.data.user.medalList = JSON.parse('[{"name":"原画","endTime":"2033-12-31 23:59:59","imgUrl":"http://img.rr.tv/screenshot/20171108/o_1510128764030.png","id":1}]');
+obj.data.user.privilegeList = JSON.parse('[{"effectObject":"video","action":"play","func":"originalPainting","description":"解锁原画","icon":"jiesuoyuanhua","endTime":1671718942000},{"effectObject":"coin","action":"sign","func":"5","description":"签到额外银币+5","icon":"qiandaoyinbi","endTime":1671718942000},{"effectObject":"growth","action":"play","func":"0.4","description":"经验值加成+40%","icon":"jingyanzhijiacheng","endTime":1671718942000},{"effectObject":"video","action":"play","func":"noLimit","description":"看剧无限制","icon":"kanjuwuxianzhi","endTime":1671718942000},{"effectObject":"video","action":"play","func":"noAd","description":"看剧无告","icon":"kanjuwuguanggao","endTime":1671718942000}]');
+JSON.stringify(obj);
