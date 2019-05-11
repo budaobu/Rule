7167b69de79130d19347cd28d758a1d81a5e9a0f
Choler <43330069+Choler@users.noreply.github.com>
2019-05-11 19:28:39 +0800

    Update KWPlayer.js

--- KWPlayer.js
+++ KWPlayer.js
@@ -1,3 +1,3 @@
 var obj = JSON.parse(body);
-obj.data = JSON.parse('{"time":"1556095726751","vipExpire":1738717749000,"biedSong":"0","isYearUser":2,"vipmExpire":1738717749000,"biedAlbum":"0","vipOverSeasExpire":1738717749000,"vip3Expire":1738717749000,"vipLuxuryExpire":1738717749000,"isNewUser":2}');
+obj.data = JSON.parse('{"time":"1514736000000","vipExpire":1640966400000,"biedSong":"0","isYearUser":2,"vipmExpire":1640966400000,"biedAlbum":"0","vipOverSeasExpire":1640966400000,"vip3Expire":1640966400000,"vipLuxuryExpire":1640966400000,"isNewUser":2}');
 JSON.stringify(obj);
